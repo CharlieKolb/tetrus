f93982c14e51a7d97f355ced37fa77b5b58e59d4
CharlieKolb <charlie.kolb@outlook.de>
2019-12-28 17:49:10 +0000

    Add blocks and moving capability

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use amethyst::{
         RenderingBundle,
     },
     utils::application_root_dir,
+    assets::{HotReloadBundle},
 };
 
 mod state;
@@ -16,21 +17,25 @@ fn main() -> amethyst::Result<()> {
 
     let app_root = application_root_dir()?;
 
-    let resources = app_root.join("resources");
+    let resources = app_root.join("resources/");
     let display_config = resources.join("display_config.ron");
 
     let game_data = GameDataBuilder::default()
         .with_bundle(TransformBundle::new())?
+        // .with_bundle(HotReloadBundle::default())? // Doesn't work? :/
         .with_bundle(
             RenderingBundle::<DefaultBackend>::new()
                 .with_plugin(
                     RenderToWindow::from_config_path(display_config)
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                        .with_clear([0., 0., 0., 1.]),
                 )
                 .with_plugin(RenderFlat2D::default()),
-        )?;
+        )?
+        .with(state::MoveBlocksSystem, "block_system", &[])
+        .with(state::BoardToRealTranslatorSystem, "board_to_real_system", &["block_system"])
+        ;
 
-    let mut game = Application::new(resources, state::MyState, game_data)?;
+    let mut game = Application::new(resources, state::TetrisGameState::default(), game_data)?;
     game.run();
 
     Ok(())
--- src/state.rs
+++ src/state.rs
@@ -1,22 +1,147 @@
 use amethyst::{
-    assets::{AssetStorage, Loader},
+    assets::{AssetStorage, Loader },
+    ecs::{Component, DenseVecStorage},
+    core::timing::Time,
     core::transform::Transform,
+    core::SystemDesc,
+    derive::SystemDesc,
     input::{get_key, is_close_requested, is_key_down, VirtualKeyCode},
     prelude::*,
+    ecs::prelude::{Join, Read, Entity, System, SystemData, World, ReadStorage, WriteStorage},
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
     window::ScreenDimensions,
 };
 
 use log::info;
 
-pub struct MyState;
+pub struct Block {
+    pub coord: (u32, u32),
+}
 
-impl SimpleState for MyState {
+impl Block {
+    fn new(x: u32, y: u32) -> Self {
+        Self {
+            coord: (x, y),
+        }
+    }
+}
+
+// impl Default for Block {
+//     fn default() -> Self {
+//         Self::new()
+//     }
+// }
+
+impl Component for Block {
+    type Storage = DenseVecStorage<Self>;
+}
+
+pub struct MovingBlock {
+    pub time_since_drop: f32, // time in seconds since last drop
+    pub time_to_drop: f32, // in blocks per second
+}
+
+impl MovingBlock {
+    fn new(blocks_per_second_drop_speed: f32) -> Self {
+        Self {
+            time_since_drop: 0.,
+            time_to_drop: 1./blocks_per_second_drop_speed,
+        }
+    }
+}
+
+impl Component for MovingBlock {
+    type Storage = DenseVecStorage<Self>;
+}
+
+pub struct Gameboard {
+    pub board: [[Option<Entity>; 10]; 24],
+    pub curr_block: Option<Entity>,
+}
+
+impl Default for Gameboard { 
+    fn default() -> Self {
+        Self {
+            board: [[None; 10]; 24],
+            curr_block: None,
+        }
+    }
+}
+
+pub struct TetrisGameState {
+    pub gameboard: Gameboard,
+    pub settings: (u32,), // todo make this a proper thing - right now only block dimension
+}
+
+impl Default for TetrisGameState {
+    fn default() -> Self {
+        Self {
+            gameboard: Gameboard::default(),
+            settings: (60,)
+        }
+    }
+}
+
+#[derive(SystemDesc)]
+pub struct MoveBlocksSystem;
+
+impl<'s> System<'s> for MoveBlocksSystem {
+    type SystemData = (
+        WriteStorage<'s, MovingBlock>,
+        WriteStorage<'s, Block>,
+        Read<'s, Time>,
+    );
+
+    fn run(&mut self, (mut moving_block, mut transform, time): Self::SystemData) {
+        let seconds = time.delta_seconds();
+        for (moving_block, block) in (&mut moving_block, &mut transform).join() {
+            moving_block.time_since_drop += seconds;
+            if moving_block.time_since_drop >= moving_block.time_to_drop {
+                if block.coord.1 == 0 {
+                    // make static block / remove moving_block
+                }
+                else {
+                    block.coord.1 -= 1;
+                }
+                moving_block.time_since_drop %= moving_block.time_to_drop;
+            }
+        }
+    }
+}
+
+#[derive(SystemDesc)]
+pub struct BoardToRealTranslatorSystem;
+
+impl<'s> System<'s> for BoardToRealTranslatorSystem {
+    type SystemData = (
+        ReadStorage<'s, Block>,
+        WriteStorage<'s, Transform>,
+    );
+
+    fn run(&mut self, (block, mut transform): Self::SystemData) {
+        let block_dimension = 16; // figure out how to read this based on state
+        for (block, transform) in (&block, &mut transform).join() {
+            transform.set_translation_xyz(
+                (block_dimension / 2 + block.coord.0 * block_dimension) as f32,
+                (block_dimension / 2 + (block.coord.1) * block_dimension ) as f32,
+                0. 
+            );
+        }
+    }
+}
+
+
+
+impl SimpleState for TetrisGameState {
     // On start will run when this state is initialized. For more
     // state lifecycle hooks, see:
     // https://book.amethyst.rs/stable/concepts/state.html#life-cycle
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
+        world.register::<Block>();
+        world.register::<MovingBlock>();
+
+        world.insert(self.settings.0);
 
         // Get the screen dimensions so we can initialize the camera and
         // place our sprites correctly later. We'll clone this since we'll
@@ -28,7 +153,33 @@ impl SimpleState for MyState {
 
         // Load our sprites and display them
         let sprites = load_sprites(world);
-        init_sprites(world, &sprites, &dimensions);
+
+
+
+        self.gameboard.curr_block = Some(world.create_entity()
+            .with(Block::new(4, 21))
+            .with(MovingBlock::new(1.))
+            .with(Transform::default())
+            .with(sprites[0].clone())
+            .build());
+
+        for i in 0..10 {
+            for j  in 0..23 {
+                world.create_entity()
+                    .with(Block::new(i, j))
+                    // .with(MovingBlock::new(1.))
+                    .with(Transform::default())
+                    .with(sprites[0].clone())
+                    .build();
+            }
+        }
+
+        // world.create_entity()
+        //     .with(Block::new())
+        //     .with(MovingBlock::new(30.))
+        //     .with(block_transform2)
+        //     .with(sprites[1].clone())
+        //     .build();
     }
 
     fn handle_event(
@@ -55,6 +206,10 @@ impl SimpleState for MyState {
         // Keep going
         Trans::None
     }
+
+    fn update(&mut self, _: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        Trans::None
+    }
 }
 
 fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
@@ -78,7 +233,7 @@ fn load_sprites(world: &mut World) -> Vec<SpriteRender> {
         let loader = world.read_resource::<Loader>();
         let texture_storage = world.read_resource::<AssetStorage<Texture>>();
         loader.load(
-            "sprites/logo.png",
+            "sprites/blocks.png",
             ImageFormat::default(),
             (),
             &texture_storage,
@@ -91,7 +246,7 @@ fn load_sprites(world: &mut World) -> Vec<SpriteRender> {
         let loader = world.read_resource::<Loader>();
         let sheet_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
         loader.load(
-            "sprites/logo.ron",
+            "sprites/blocks.ron",
             SpriteSheetFormat(texture_handle),
             (),
             &sheet_storage,
@@ -101,7 +256,7 @@ fn load_sprites(world: &mut World) -> Vec<SpriteRender> {
     // Create our sprite renders. Each will have a handle to the texture
     // that it renders from. The handle is safe to clone, since it just
     // references the asset.
-    (0..3)
+    (0..2)
         .map(|i| SpriteRender {
             sprite_sheet: sheet_handle.clone(),
             sprite_number: i,
@@ -109,22 +264,22 @@ fn load_sprites(world: &mut World) -> Vec<SpriteRender> {
         .collect()
 }
 
-fn init_sprites(world: &mut World, sprites: &[SpriteRender], dimensions: &ScreenDimensions) {
-    for (i, sprite) in sprites.iter().enumerate() {
-        // Center our sprites around the center of the window
-        let x = (i as f32 - 1.) * 100. + dimensions.width() * 0.5;
-        let y = (i as f32 - 1.) * 100. + dimensions.height() * 0.5;
-        let mut transform = Transform::default();
-        transform.set_translation_xyz(x, y, 0.);
+// fn init_sprites(world: &mut World, sprites: &[SpriteRender], dimensions: &ScreenDimensions) {
+//     for (i, sprite) in sprites.iter().enumerate() {
+//         // Center our sprites around the center of the window
+//         let x = (i as f32 - 1.) * 100. + dimensions.width() * 0.5;
+//         let y = (i as f32 - 1.) * 100. + dimensions.height() * 0.5;
+//         let mut transform = Transform::default();
+//         transform.set_translation_xyz(x, y, 0.);
 
-        // Create an entity for each sprite and attach the `SpriteRender` as
-        // well as the transform. If you want to add behaviour to your sprites,
-        // you'll want to add a custom `Component` that will identify them, and a
-        // `System` that will iterate over them. See https://book.amethyst.rs/stable/concepts/system.html
-        world
-            .create_entity()
-            .with(sprite.clone())
-            .with(transform)
-            .build();
-    }
-}
+//         // Create an entity for each sprite and attach the `SpriteRender` as
+//         // well as the transform. If you want to add behaviour to your sprites,
+//         // you'll want to add a custom `Component` that will identify them, and a
+//         // `System` that will iterate over them. See https://book.amethyst.rs/stable/concepts/system.html
+//         world
+//             .create_entity()
+//             .with(sprite.clone())
+//             .with(transform)
+//             .build();
+//     }
+// }
